0e437f473e072fcbfde0826bec90ce28b0c4fb70
praptix <choudharyprapti7@gmail.com>
2024-10-02 10:49:38 +0530

    Update intermediate.py

--- intermediate.py
+++ intermediate.py
@@ -1,72 +1,62 @@
-# import necessary modules
-
-# for implementing the HTTP Web servers
 import http.server
-
-# provides access to the BSD socket interface
-import socket
-
-# a framework for network servers
 import socketserver
-
-# to display a Web-based documents to users
+import pyqrcode
+import png
+import os
+import socket
 import webbrowser
 
-# to generate qrcode
-import pyqrcode
-from pyqrcode import QRCode
+# Define port number and user name
+PORT = 8000  # Change this if needed
+USER_NAME = "YourName"  # Replace with your name
 
-# convert into png format
-import png
+# Step 1: Get IP address of the machine
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # Connect to a remote host to get the local IP address
+        s.connect(('10.254.254.254', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'  # Default to localhost if there's an error
+    finally:
+        s.close()
+    return ip
 
-# to access operating system control
-import os
+# Step 2: Generate QR code for easy access
+def generate_qr_code(ip, port):
+    url = f"http://{ip}:{port}"
+    qr = pyqrcode.create(url)
+    qr.png("qrcode.png", scale=6)  # Save QR code as an image
+    print(f"QR code generated: {url}")
+    return url
 
+# Step 3: HTTP Request Handler to handle file sharing
+class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        # Display custom message in the browser
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><body><h1>File Sharing App</h1>", "utf-8"))
+        self.wfile.write(bytes(f"<p>Welcome, {USER_NAME}! You can share your files here.</p>", "utf-8"))
+        super().do_GET()  # Call the default GET handler to list files
 
-# assigning the appropriate port value
-PORT = 8010
-# this finds the name of the computer user
-os.environ['USERPROFILE']
+# Step 4: Set up the HTTP server
+def run_server():
+    handler = CustomHTTPRequestHandler
+    with socketserver.TCPServer(("", PORT), handler) as httpd:
+        print(f"Serving at port {PORT}")
+        httpd.serve_forever()
 
+# Step 5: Open the QR code image in a web browser
+def open_qr_code():
+    webbrowser.open("qrcode.png")
 
-# changing the directory to access the files desktop
-# with the help of os module
-desktop = os.path.join(os.path.join(os.environ['USERPROFILE']),
-					'OneDrive')
-os.chdir(desktop)
-
-
-# creating a http request
-Handler = http.server.SimpleHTTPRequestHandler
-# returns, host name of the system under
-# which Python interpreter is executed
-hostname = socket.gethostname()
-
-
-# finding the IP address of the PC
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-IP = "http://" + s.getsockname()[0] + ":" + str(PORT)
-link = IP
-
-
-# converting the IP address into the form of a QRcode
-# with the help of pyqrcode module
-
-# converts the IP address into a Qrcode
-url = pyqrcode.create(link)
-# saves the Qrcode inform of svg
-url.svg("myqr.svg", scale=8)
-# opens the Qrcode image in the web browser
-webbrowser.open('myqr.svg')
-
-
-# Creating the HTTP request and serving the
-# folder in the PORT 8010,and the pyqrcode is generated
-
-# continuous stream of data between client and server
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-	print("serving at port", PORT)
-	print("Type this in your Browser", IP)
-	print("or Use the QRCode")
-	httpd.serve_forever()
+# Step 6: Main function to run the entire application
+if __name__ == "__main__":
+    ip = get_ip()  # Get the local IP address of the machine
+    url = generate_qr_code(ip, PORT)  # Generate and display QR code
+    open_qr_code()  # Open the generated QR code image in the default web browser
+    run_server()  # Start the HTTP server to share files
